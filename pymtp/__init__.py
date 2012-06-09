694d04e289a4504ebb6733a545b22b00c0d93f87
M. Dietrich <mdt@emdete.de>
2012-06-09 13:03:39 +0200

    adjust main: version, import, developer

--- pymtp/__init__.py
+++ pymtp/__init__.py
@@ -2,32 +2,28 @@
 # -*- coding: iso-8859-1 -*-
 #
 # PyMTP
-# Developed by: Nick Devito (nick@nick125.com)
+# Developed by: Nick Devito <nick@nick125.com>
 # (c) 2008 Nick Devito
 # Released under the GPLv3 or later.
 #
 
 """
-	PyMTP is a pythonic wrapper around libmtp, making it a bit more 
-	friendly to use in python
+	PyMTP is a pythonic wrapper around libmtp, making it a bit more
+	friendly to be used in python
 
 	Example Usage (or see examples/):
 		>>> import pymtp
-		>>> mtp = pymtp.MTP()
-	        >>> mtp.connect()
-		PTP: Opening session
-	        >>> print mtp.get_devicename()
-	        Device name
-	        >>> mtp.disconnect()
-		PTP: Closing session
+		>>> with = pymtp.MTP() as mtp:
+		>>>     print mtp.get_devicename()
 		>>>
 """
 
-__VERSION__ = "0.1.0"
-__VERSION_MACRO__ = 0
+__VERSION_MACRO__ = 1
 __VERSION_MINOR__ = 1
 __VERSION_MAJOR__ = 0
 __VERSION_TUPLE__ = (__VERSION_MAJOR__, __VERSION_MINOR__, __VERSION_MACRO__)
-__AUTHOR__ = "Nick Devito (nick@nick125.com)"
-__LICENSE__ = "GPL-3"
+__VERSION__ = "{}.{}.{}".format(*__VERSION_TUPLE__)
+__AUTHOR__ = "Nick Devito <nick@nick125.com>, M. Dietrich <mdt@pyneo.org>"
+__LICENSE__ = "GPLv3"
 
+from main import MTP
